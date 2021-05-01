0549599b1bed058728e9e3027e922a3c9f5f834b
Andrei Grigoras <andrei.grigoras2015@gmail.com>
2021-05-01 07:07:27 -0700

    tuning

--- server/install.sh
+++ server/install.sh
@@ -46,9 +46,13 @@ echo -e $GREEN"[+] Apache2 up and running."$ANSII_END
 
 
 # Download all the necessary files from the Github repository.
-check_status curl --silent https://raw.githubusercontent.com/KaynRO/Proiect-Licenta/main/server/server.sh?token=AEK5VX6OLAZ6ZFIVYSSMQZ3ARVNXQ > /root/server.sh $RED"[!] server.sh script failed while downloading!"$ANSII_END
-check_status curl --silent https://raw.githubusercontent.com/KaynRO/Proiect-Licenta/main/client/blacklister.sh?token=AEK5VX7EL2EXBC34FCXUA4TARVNZ4 > /var/www/html/blacklister.sh $RED"[!] blacklister.sh script failed while downloading!"$ANSII_END
-check_status curl --silent https://raw.githubusercontent.com/KaynRO/Proiect-Licenta/main/client/reporter.sh?token=AEK5VX5SGEPUI4DAOZML2YTARVN2O > /var/www/html/reporter.sh $RED"[!] reporter.sh script failed while downloading!"$ANSII_END
+#check_status curl --silent https://raw.githubusercontent.com/KaynRO/Proiect-Licenta/main/server/server.sh?token=AEK5VX6OLAZ6ZFIVYSSMQZ3ARVNXQ > /root/server.sh $RED"[!] server.sh script failed while downloading!"$ANSII_END
+#check_status curl --silent https://raw.githubusercontent.com/KaynRO/Proiect-Licenta/main/client/blacklister.sh?token=AEK5VX7EL2EXBC34FCXUA4TARVNZ4 > /var/www/html/blacklister.sh $RED"[!] blacklister.sh script failed while downloading!"$ANSII_END
+#check_status curl --silent https://raw.githubusercontent.com/KaynRO/Proiect-Licenta/main/client/reporter.sh?token=AEK5VX5SGEPUI4DAOZML2YTARVN2O > /var/www/html/reporter.sh $RED"[!] reporter.sh script failed while downloading!"$ANSII_END
+cd /tmp ; git clone https://github.com/KaynRO/Proiect-Licenta ; cd Proiect-Licenta
+cp /server/server.sh /root/server.sh
+cp /client/blacklister.sh /var/www/html/blacklister.sh
+cp /client/reporter.sh /var/www/html/reporter.sh
 echo -e $GREEN"[+] Files successfully downloaded."$ANSII_END
 
 
