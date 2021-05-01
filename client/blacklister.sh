bf1a87747f472f659888b966fbfa22cce14729f2
Andrei Grigoras <andrei.grigoras2015@gmail.com>
2021-05-01 09:47:17 -0700

    tuning

--- client/blacklister.sh
+++ client/blacklister.sh
@@ -87,14 +87,14 @@ do
 	# Check every second for new fles on the system. As users should only have write access to their home directory ONLY, monitor that directory only.
 	# This can be easiliy changed to match multiple directories or match all except some (-not -path)
 	new_files=`find /home	-ignore_readdir_race -type f \
-							-mmin -0.1`
+							-cmin -0.01`
 
 	# Submit them to HybridAnalysis by spawning the function in the background.
 	for i in $new_files
 	do
 		process_file $i &
 	done
-	
-	sleep 6
+
+	sleep 1
 done
 
