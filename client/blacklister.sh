6c195b438a9e73165150bb839db378d7073b4f76
Andrei Grigoras <andrei.grigoras2015@gmail.com>
2021-05-01 06:08:35 -0700

    ready for first test

---
+++ client/blacklister.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+
+# Define API Key to be used for HybridAnalysis, Logfile, Quarantine directory as well as system hostname.
+API_KEY="y8h1wkc8fd8afc85d3shu7foa91f4d63mu2hkeji0aa425dbdrevqgfzc878cafd"
+HOSTNAME=`hostname`
+LOGFILE="/root/log.txt"
+QUARANTINE="/root/quarantine"
+
+
+function init(){
+	# Create the quarantine directory as well as the log file. To avoid any change of the logfile, which could cause
+	# malware not being detected, set file attribute to immutable. This can also be used as a syncronization mechanism
+	mkdir $QUARANTINE 2> /dev/null
+	chmod -R 660 $QUARANTINE
+	touch $LOGFILE 2> /dev/null
+	chattr +i $LOGFILE
+
+}
+
+
+function isolate_file(){
+	# If file is malicious or suspicious, move them to a temp folder, change owner to root and change permissions.
+	# Also, make the file immutable to assure no one ever touches it in any way.
+	mv $1 $2
+	chown root:root $fn
+	chmod 600 $2
+	chattr +i $2
+}
+
+
+function process_file(){
+	# Construct the curl request to the API using the official schema.
+	base=`hostname`_`date +'%s'`
+	resp=`curl -X POST --silent \
+					-H "User-Agent: Falcon Sandbox" \
+					-H "Accept: application/json" \
+					-H "Content-Type: multipart/form-data" \
+					-H "Api-Key: $API_KEY" \
+					-F "scan_type=all" \
+					-F "no_share_third_party=false" \
+					-F "allow_community_access=true" \
+					-F "submit_name=$base_$1" \
+					-F "file=@$1" \
+					"https://www.hybrid-analysis.com/api/v2/quick-scan/file"`
+
+	# If file is not empty.
+	if [[ $resp != *"An empty file is not allowed"* ]]
+	then
+		id=`echo $resp | jq .id | sed 's/\"//g'`
+		finished=`echo $resp | jq .finished`
+		sha256=`echo $resp | jq .sha256 | sed 's/\"//g'`
+
+		# If file has not been fully analyzed yet, retry until it's done.
+		while [[ "$finished" == "false" ]]
+		do
+			sleep 0.1
+			resp=`curl -X GET --silent \
+					-H "User-Agent: Falcon Sandbox" \
+					-H "Accept: application/json" \
+					-H "Api-Key: $API_KEY" \
+					"https://www.hybrid-analysis.com/api/v2/quick-scan/$id"`
+			finished=`echo $resp | jq .finished`
+		done
+
+		# See if any of the scanners responded with 'suspicious' or 'malicious' and isolate the file is so. 
+		# To append to logfile, delete immutable attribute first, append then add it back.
+		malicious=`echo $resp | jq '.scanners[].status | contains("malicious")'`
+		suspicious=`echo $resp | jq '.scanners[].status | contains("suspicious")'`
+		if [[ $malicious == *"true"* ]] || [[ $suspicious == *"true"* ]]
+		then
+
+			fn="$QUARANTINE/$base"_`echo $1 | awk -F '/' '{print $NF}'`
+			chattr -i $LOGFILE
+			echo -e "[+] New suspicious/malicious file: $1\n    Scan URL: https://www.hybrid-analysis.com/sample/$sha256\n    Quaratined: $fn" >> $LOGFILE
+			chattr +i $LOGFILE
+			isolate_file $1 $fn
+		fi
+	fi
+}
+
+
+init
+
+while true
+do
+	# Check every second for new fles on the system. As users should only have write access to their home directory ONLY, monitor that directory only.
+	# This can be easiliy changed to match multiple directories or match all except some (-not -path)
+	new_files=`find /home	-ignore_readdir_race -type f \
+							-cmin -0.01`
+
+	# Submit them to HybridAnalysis by spawning the function in the background.
+	for i in $new_files
+	do
+		process_file $i &
+	done
+
+	# Sleep for one second to avoid duplicate files detection.
+	sleep 1
+done
+
--- client/reporter.sh
+++ client/reporter.sh
@@ -7,33 +7,28 @@ SERVER_PORT=$2
 
 
 # Define the logfile where the script that detects malware will output it's results
-LOG_FILE="./log.txt"
+LOGFILE="/root/log.txt"
 
 
 # A temporary files that hold the newly added lines to the logfile
 TMP_FILE='/tmp/.new_lines.tmp'
 
 
-# Keep a count on the number of lines of the logfile from the last inspection
-old_line_count=`wc -l $LOG_FILE | cut -c1`
-
-
 while true
 do
-	# Calculate the current number of lines of the logfile
-	new_line_count=`wc -l $LOG_FILE | cut -c1`
+	# Get the first 3 lines from the logfile (equivalent to one file entry) and put the information
+	# in a tmp file.
+	lines=`head -n 3 $LOGFILE`
+	echo -e `hostname -i\n``hostname\n`$lines > $TMP_FILE
 
-	if [[ $new_line_count != $old_line_count ]]
-	then
-		# If something was added, old != new, then extract the new lines and send them over to the server.
-		diff_lines="$(($new_line_count - $old_line_count))"
-		echo `hostname -i` `hostname` `tail -n$diff_lines $LOG_FILE` > $TMP_FILE
-		nc -w 3 $SERVER_IP $SERVER_PORT < $TMP_FILE
-		sleep 5
+	# Remove immutable attribute, move the first 3 lines of the logfile to the end and make it immutable again.
+	chattr -i $LOGFILE
+	sed -i '1,2,3d' $LOGFILE
+	echo $lines >> $LOGFILE
+	chattr +i $LOGFILE
 
-	fi
-
-	# Update current line count
-	old_line_count=$new_line_count
+	# Send the tmp file to the server and cause a small delay.
+	nc -w 3 $SERVER_IP $SERVER_PORT < $TMP_FILE
+	sleep 1
 done
 
