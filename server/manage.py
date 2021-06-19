0c885deace5f7a49fb95f23e08c2a8cfddc19982
Kayn <andrei.grigoras2015@gmail.com>
2021-06-19 18:42:44 +0300

    adding server web application code

---
+++ server/manage.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask.templating import render_template
+import os
+from flask.wrappers import Response
+
+# Initialize Flask App
+app = Flask(__name__)
+ 
+@app.route('/')
+def create():
+    return render_template("index.html")
+
+@app.route('/data',methods = ['GET'])
+def readData():
+    f = open("/root/log.txt", "r")
+    data = str(f.read())
+    res = Response(data,mimetype="text/xml")
+    return res
+
+port = int(os.environ.get('PORT', 80))
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
